bd0fc84d97b55d4ba9dfc77dd6cbccf8297a69b7
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-05-21 18:14:36 +0700

    Mmebuat kelas mahasiswa dalam memberlistinit

--- Memberlistinit/Memberlistinit.cpp
+++ Memberlistinit/Memberlistinit.cpp
@@ -9,25 +9,3 @@ using namespace std;
 //Minimal commit = 15
 
 class mahasiswa {
-private:
-	const int id;
-	string nama;
-	float nilai;
-
-public:
-	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
-		//Member definisi
-	}
-
-	~mahasiswa() {
-		cout << "Id = " << id << endl;
-		cout << "Nama = " << nama << endl;
-		cout << "Nilai = " << nilai << endl;
-	}
-};
-
-int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
-
-	return 0;
-}
