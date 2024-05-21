1b7931ed31c3ef2322bdfda8fdf5342cea557953
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-05-21 18:23:47 +0700

    Membuat kelas manusia pada manusia.h dalam relasi composisi

--- Composition/manusia.h
+++ Composition/manusia.h
@@ -3,16 +3,5 @@
 using namespace std;
 
 class manusia {
-public:
-	string name;
-	jantung varjantung;
 
-	manusia(string pName)
-		: name(pName) {
-		cout << name << " hidup\n";
-	}
-	~manusia() {
-		cout << name << " mati\n";
-	}
-};
 #endif
